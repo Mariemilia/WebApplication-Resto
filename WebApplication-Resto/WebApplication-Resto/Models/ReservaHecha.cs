0f021a37f312221db37b03a611267d09480000da
María Emilia Olcese <olcesemariaemilia@gmail.com>
2022-06-22 18:20:59 -0300

    Arreglo de atributos de Reserva

--- WebApplication-Resto/WebApplication-Resto/Models/ReservaHecha.cs
+++ WebApplication-Resto/WebApplication-Resto/Models/ReservaHecha.cs
@@ -1,16 +1,21 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using static WebApplication_Resto.Models.Validations;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace WebApplication_Resto.Models
 {
     public class ReservaHecha : Reserva
     {
-        [Display(Name = "Fecha de la reserva:")]
-        public string FechaR { get; set; }
-
+        [Display(Name = "Fecha y hora de la reserva:")]
+        public DateTime FechaR { get; set; }
         [Display(Name = "Cantidad de mesas reservadas: ")]
         public int MesasReservadas { get; set; }
-        
+
+        [DniExistsDB]
         [Display(Name = "DNI del titular de la reserva: ")]
-        public int DniTitular { get; set; }
-    }
+        public int DniTitular { get; set; } }
+       // public IEnumerable<SelectListItem> ReservaItems { get; set; }
+
 }
